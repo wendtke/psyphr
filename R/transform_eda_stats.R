73e6e6436ee02d62fc77778a6e6d041d951a04ed
Kathleen Wendt <wendtke@colostate.edu>
2019-04-16 13:09:56 -0500

    create transform_eda_sheet function

--- R/transform_eda_stats.R
+++ R/transform_eda_stats.R
@@ -1,26 +1,39 @@
-
-## ----------------- input and tidy each file within function -----------------
-
-read_eda_file <- function(file){
-  print(file)
-  read_xlsx(file, sheet = 1) %>%
+#' Process "EDA Stats" sheet
+#'
+#' The transform_eda_sheet function ...
+#'
+#' @param file_name
+#' @param sheet
+#' @param rename_columns
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+transform_eda_sheet <- function(file_name, sheet, rename_columns = TRUE){
+  sheet_base <- sheet %>%
     gather(-`Segment Number`,
            key = "segment",
            value = "value") %>%
     spread(key = `Segment Number`,
            value = value) %>%
-    select(-`End Event`,
-           -`Start Event`) %>%
-    rename(segment = segment,
-           start_time = `Start Time`,
-           end_time = `End Time`,
-           seg_length = `Segment Duration`,
-           total_scr = `Total SCRs`,
-           er_scr = `ER-SCRs`,
-           ns_scrs = `NS-SCRs`,
-           tonic_scl = `Tonic SCL`,
-           mean_sc = `Mean SC`,
-           tonic_period = `Tonic Period`) %>%
     mutate_all(as.numeric) %>%
-    mutate(file_name = sub(".xlsx", "", basename(file)))
+    mutate(file_name = file_name)
+  # mutate(file_name = sub(".xlsx", "", basename(file_name)))
+
+  if(rename_columns){
+    sheet_base <- sheet_base %>%
+      rename(segment = segment,
+             start_time = `Start Time`,
+             end_time = `End Time`,
+             seg_length = `Segment Duration`,
+             total_scr = `Total SCRs`,
+             er_scr = `ER-SCRs`,
+             ns_scrs = `NS-SCRs`,
+             tonic_scl = `Tonic SCL`,
+             mean_sc = `Mean SC`,
+             tonic_period = `Tonic Period`)
+  }
 }
+
