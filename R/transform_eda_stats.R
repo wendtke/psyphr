00311e0da1d6d0b98943e5057bf45020d93cdafe
Kathleen Wendt <wendtke@colostate.edu>
2019-04-16 13:30:20 -0500

    update transform_eda_sheet after extracting rename()

--- R/transform_eda_stats.R
+++ R/transform_eda_stats.R
@@ -1,39 +1,28 @@
 #' Process "EDA Stats" sheet
 #'
-#' The transform_eda_sheet function ...
+#' The transform_eda_sheet function transforms the raw EDA Stats sheet to use
+#' the Segment column names, make all values numeric, and add the name of the
+#' file as a column, so the data includes the source designation.
 #'
 #' @param file_name
 #' @param sheet
-#' @param rename_columns
 #'
-#' @return
+#' @return a dataframe of segment data with a column containing the file name
 #' @export
 #'
 #' @examples
+#' transform_eda_sheet(name_of_file, sheet_data)
 #'
-transform_eda_sheet <- function(file_name, sheet, rename_columns = TRUE){
-  sheet_base <- sheet %>%
-    gather(-`Segment Number`,
+transform_eda_sheet <- function(file_name, sheet){
+  sheet <- sheet %>%
+    tidyr::gather(-`Segment Number`,
            key = "segment",
            value = "value") %>%
-    spread(key = `Segment Number`,
+    tidyr::spread(key = `Segment Number`,
            value = value) %>%
-    mutate_all(as.numeric) %>%
-    mutate(file_name = file_name)
-  # mutate(file_name = sub(".xlsx", "", basename(file_name)))
+    dplyr::mutate_all(as.numeric) %>%
+    dpylr::mutate(file_name = file_name)
 
-  if(rename_columns){
-    sheet_base <- sheet_base %>%
-      rename(segment = segment,
-             start_time = `Start Time`,
-             end_time = `End Time`,
-             seg_length = `Segment Duration`,
-             total_scr = `Total SCRs`,
-             er_scr = `ER-SCRs`,
-             ns_scrs = `NS-SCRs`,
-             tonic_scl = `Tonic SCL`,
-             mean_sc = `Mean SC`,
-             tonic_period = `Tonic Period`)
+  sheet
+
   }
-}
-
