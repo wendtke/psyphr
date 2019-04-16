7b1cff10b702927e089f132a53d85cb79a65a3dd
Kathleen Wendt <wendtke@colostate.edu>
2019-04-16 16:24:37 -0500

    align code

--- R/files.R
+++ R/files.R
@@ -4,17 +4,25 @@ workbook_sheets <- function(pathname, extension){
 
   # Find files in directory
 
-  files_full_path <- list.files(path = path_name, pattern = extension, full.names = T)
-  file_names <- list.files(path = path_name, pattern = extension, full.names = FALSE)
-  file_names <- gsub(pattern = extension, replacement = "", x = file_names)
+  files_full_path <- list.files(path = path_name,
+                                pattern = extension,
+                                full.names = TRUE)
+  file_names <- list.files(path = path_name,
+                           pattern = extension,
+                           full.names = FALSE)
+  file_names <- gsub(pattern = extension,
+                     replacement = "", x = file_names)
 
-  #Make names for dataframes
+  # Make names for dataframes
 
   df_names <- list()
   for(i in 1:length(files_full_path)){
     sheet_names <- excel_sheets(files_full_path[i])
-    df_names[[i]] <- paste(file_names[i], sheet_names, sep = "_")
-    df_names[[i]] <- gsub(pattern = " ", replacement = "_", x = df_names[[i]])
+    df_names[[i]] <- paste(file_names[i],
+                           sheet_names, sep = "_")
+    df_names[[i]] <- gsub(pattern = " ",
+                          replacement = "_",
+                          x = df_names[[i]])
   }
   df_names <- unlist(df_names)
 
@@ -26,7 +34,8 @@ workbook_sheets <- function(pathname, extension){
     sheet_names <- excel_sheets(files_full_path[i])
 
     for(j in 1:length(sheet_names)){
-      list_of_wkbk_sheets[[list_item]] <- as.data.frame(read_excel(files_full_path[i], sheet = sheet_names[j]))
+      list_of_wkbk_sheets[[list_item]] <- as.data.frame(read_excel(files_full_path[i],
+                                                                   sheet = sheet_names[j]))
       list_item <- list_item + 1
     }
   }
