9521446368c1d86ecd1e8ef58da3a3b9ec891cbb
Kathleen Wendt <wendtke@colostate.edu>
2019-04-16 13:43:52 -0500

    add hrv and eda variable names in rename.R

--- R/rename.R
+++ R/rename.R
@@ -22,26 +22,57 @@
 #'
 #'
 
-rename_hrv_editing_columns <- function(sheet) {
-    sheet <- sheet %>%
+
+
+rename_hrv_stats_columns <- function(sheet){
+  sheet <- sheet %>%
     dplyr::rename(segment = segment,
-           ecg_sec_cut = `ECG : Seconds Removed`,
-           ecg_perc_cut = `ECG : Percentage Removed`,
-           ecg_sec_est = `ECG : Seconds Estimated`,
-           ecg_perc_est = `ECG : Percentage Estimated`,
-           resp_sec_cut = `Resp : Seconds Removed`,
-           resp_perc_cut = `Resp : Percentage Removed`,
-           resp_sec_est = `Resp : Seconds Estimated`,
-           resp_perc_est = `Resp : Percentage Estimated`,
-           total_r_peaks = `Total Peaks`,
-           norm_r_peaks = `Normal Peaks`,
-           norm_r_perc = `% Normal Peaks`,
-           est_r_peaks = `Estimated Peaks`,
-           est_r_peaks_perc = `% Estimated Peaks`,
-           art_r_peaks = `Artifact Peaks`,
-           art_r_peaks_perc = `% Artifact Peaks`,
-           dur_est_r_r = `Duration of Estimated R-R Intervals`,
-           est_r_r_perc = `% of Estimated R-R Intervals`)
+                  start_time = `Start Time`,
+                  end_time = `End Time`,
+                  seg_length = `Segment Duration`,
+                  mean_hr = `Mean Heart Rate`,
+                  rsa = RSA,
+                  mean_ibi = `Mean IBI`,
+                  r_peaks = `# of R's Found`,
+                  resp_rate = `Respiration Rate`,
+                  resp_amp = `Respiration Amplitude`,
+                  resp_peak_freq = `Respiration Peak Frequency`,
+                  resp_power = `Respiration Power`,
+                  first_r_time = `First ECG R Time`,
+                  last_r_time = `Last ECG R Time`,
+                  r_r_duration = `First R to Last R Duration`,
+                  sdnn = SDNN,
+                  avnn = AVNN,
+                  rmssd = RMSSD,
+                  nn50 = NN50,
+                  pnn50 = pNN50)
+
+  sheet
+
+}
+
+
+
+rename_hrv_editing_columns <- function(sheet) {
+  sheet <- sheet %>%
+    dplyr::rename(segment = segment,
+                  ecg_sec_cut = `ECG : Seconds Removed`,
+                  ecg_perc_cut = `ECG : Percentage Removed`,
+                  ecg_sec_est = `ECG : Seconds Estimated`,
+                  ecg_perc_est = `ECG : Percentage Estimated`,
+                  resp_sec_cut = `Resp : Seconds Removed`,
+                  resp_perc_cut = `Resp : Percentage Removed`,
+                  resp_sec_est = `Resp : Seconds Estimated`,
+                  resp_perc_est = `Resp : Percentage Estimated`,
+                  total_r_peaks = `Total Peaks`,
+                  norm_r_peaks = `Normal Peaks`,
+                  norm_r_perc = `% Normal Peaks`,
+                  est_r_peaks = `Estimated Peaks`,
+                  est_r_peaks_perc = `% Estimated Peaks`,
+                  art_r_peaks = `Artifact Peaks`,
+                  art_r_peaks_perc = `% Artifact Peaks`,
+                  dur_est_r_r = `Duration of Estimated R-R Intervals`,
+                  est_r_r_perc = `% of Estimated R-R Intervals`)
 
   sheet
 
@@ -50,17 +81,32 @@ rename_hrv_editing_columns <- function(sheet) {
 
 rename_eda_stats_columns <- function(sheet){
   sheet <- sheet %>%
-  dpylr::rename(segment = segment,
-           start_time = `Start Time`,
-           end_time = `End Time`,
-           seg_length = `Segment Duration`,
-           total_scr = `Total SCRs`,
-           er_scr = `ER-SCRs`,
-           ns_scrs = `NS-SCRs`,
-           tonic_scl = `Tonic SCL`,
-           mean_sc = `Mean SC`,
-           tonic_period = `Tonic Period`)
+    dpylr::rename(segment = segment,
+                  start_time = `Start Time`,
+                  end_time = `End Time`,
+                  seg_length = `Segment Duration`,
+                  total_scr = `Total SCRs`,
+                  er_scr = `ER-SCRs`,
+                  ns_scrs = `NS-SCRs`,
+                  tonic_scl = `Tonic SCL`,
+                  mean_sc = `Mean SC`,
+                  tonic_period = `Tonic Period`)
 
   sheet
 
+}
+
+
+
+rename_eda_editing_columns <- function(sheet){
+  sheet <- sheet %>%
+    dplyr::rename(segment = segment,
+                  eda_sec_cut = `EDA : Seconds Removed`,
+                  eda_perc_cut = `EDA : Percentage Removed`,
+                  eda_sec_est = `EDA : Seconds Estimated`,
+                  eda_perc_est = `EDA : Percentage Estimated`,
+                  resp_sec_cut = `Resp : Seconds Removed`,
+                  resp_perc_cut = `Resp : Percentage Removed`,
+                  resp_sec_est = `Resp : Seconds Estimated`,
+                  resp_perc_est = `Resp : Percentage Estimated`)
 }
