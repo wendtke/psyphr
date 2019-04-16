3bb31226a3b6d02aebc9078ac4231433bf445f5a
Kathleen Wendt <wendtke@colostate.edu>
2019-04-16 13:35:58 -0500

    clean up transform_hrv_sheet; move rename to rename.R

--- R/transform_hrv_stats.R
+++ R/transform_hrv_stats.R
@@ -1,48 +1,28 @@
 #' Process "HRV Stats" sheet
 #'
-#' The transform_hrv_sheet function ...
+#' The transform_hrv_sheet function transforms the raw HRV Stats sheet to
+#' use the Segment Names as column names, make all values numeric, and add the
+#' name of the file as a column, so the data includes the source designation.
 #'
 #' @param file_name
 #' @param sheet
-#' @param rename_columns
 #'
-#' @return
+#' @return a dataframe of segment data with a column containing file name
 #' @export
 #'
 #' @examples
+#' transform_hrv_sheet(name_of_file, sheet_data)
 #'
-transform_hrv_sheet <- function(file_name, sheet, rename_columns = TRUE){
-  sheet_base <- sheet %>%
-    gather(-`Segment Number`,
+transform_hrv_sheet <- function(file_name, sheet){
+  sheet <- sheet %>%
+    tidyr::gather(-`Segment Number`,
            key = "segment",
            value = "value") %>%
-    spread(key = `Segment Number`,
+    tidyr::spread(key = `Segment Number`,
            value = value) %>%
-    mutate_all(as.numeric) %>%
-    mutate(file_name = file_name)
-  # mutate(file_name = sub(".xlsx", "", basename(file_name)))
+    dplyr::mutate_all(as.numeric) %>%
+    dplyr::mutate(file_name = file_name)
 
-  if(rename_columns){
-    sheet_base <- sheet_base %>%
-      rename(segment = segment,
-             start_time = `Start Time`,
-             end_time = `End Time`,
-             seg_length = `Segment Duration`,
-             mean_hr = `Mean Heart Rate`,
-             rsa = RSA,
-             mean_ibi = `Mean IBI`,
-             r_peaks = `# of R's Found`,
-             resp_rate = `Respiration Rate`,
-             resp_amp = `Respiration Amplitude`,
-             resp_peak_freq = `Respiration Peak Frequency`,
-             resp_power = `Respiration Power`,
-             first_r_time = `First ECG R Time`,
-             last_r_time = `Last ECG R Time`,
-             r_r_duration = `First R to Last R Duration`,
-             sdnn = SDNN,
-             avnn = AVNN,
-             rmssd = RMSSD,
-             nn50 = NN50,
-             pnn50 = pNN50)
+  sheet
+
   }
-}
