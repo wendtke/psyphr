3bb4a639ffb3333910c77968b2ea06a465aab284
Siqi Zhang <iqis.gnahz@gmail.com>
2019-07-22 19:06:51 -0400

    + sample_study_mw()

---
+++ R/utils.R
@@ -0,0 +1,8 @@
+#' Mindware Sample Study
+#'
+#' @return path to sample MindWare study; character string
+#' @export
+#'
+sample_study_MW <- function(){
+  system.file("extdata/MW/study", package = "psyphr")
+}
